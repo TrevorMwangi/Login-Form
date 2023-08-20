1368ebca49e838fae98e20da791be26733debf28
Trevor <82081705+TrevorMwangi@users.noreply.github.com>
2023-08-20 23:38:56 +0300

    Update loginpage.py

--- loginpage.py
+++ loginpage.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox
+from tkinter import ttk
 
 def perform_login():
     username = username_entry.get()
@@ -18,16 +19,17 @@ username_label = tk.Label(root, text="Username:", font= ("Arial", 13))
 username_label.pack()
 
 username_entry = tk.Entry(root, font=("Arial", 12), width=30)
-username_entry.pack()
+username_entry.pack(pady=5)
 
 password_label = tk.Label(root, text="Password:", font=("Arial", 13))
-password_label.pack()
+password_label.pack(pady=5)
 
 password_entry = tk.Entry(root, show="*", font=("Arial", 12), width=30)
-password_entry.pack()
+password_entry.pack(pady=5)
 
-login_button = tk.Button(root, text="Login", command=perform_login)
-login_button.pack()
+login_button = ttk.Button(root, text="Login", command=perform_login)
+login_button.pack(padx=5)
+
 
 # Bind the Enter key event to the perform_login function
 root.bind("<Return>", lambda event=None: perform_login())
