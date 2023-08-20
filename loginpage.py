9c893571ec29acd07aa01d24a8ed44079baf0915
Trevor <82081705+TrevorMwangi@users.noreply.github.com>
2023-08-20 23:10:32 +0300

    Update loginpage.py
    Login form, with enter key command

--- loginpage.py
+++ loginpage.py
@@ -1,32 +1,36 @@
 import tkinter as tk
 from tkinter import messagebox
 
-def login():
+def perform_login():
     username = username_entry.get()
     password = password_entry.get()
 
-    if username == "Trevor Mwangi" and password == "passwordis123":
-        messagebox.showinfo("Login Success!", "Welcome, Trevor!")
+    if username == "Trevor" and password == "password123":
+        messagebox.showinfo("Login Successful", "Welcome, {}!".format(username))
     else:
-        messagebox.showerror("Login Failed", "Invalid username or password!")
+        messagebox.showerror("Login Failed!", "Invalid username or password")
 
-#  main window
-window = tk.Tk()
-window.title("LOGIN")
-window.geometry("500x300")
+root = tk.Tk()
+root.title("Login Form")
+root.geometry("500x300")
 
-username_label = tk.Label(window, text="Username:", font=("Arial", 13))
+username_label = tk.Label(root, text="Username:", font= ("Arial", 13))
 username_label.pack()
-username_entry = tk.Entry(window, font=("Arial", 12), width=30)
+
+username_entry = tk.Entry(root, font=("Arial", 12), width=30)
 username_entry.pack()
 
-password_label = tk.Label(window, text="Password:", font=("Arial", 13))
+password_label = tk.Label(root, text="Password:", font=("Arial", 13))
 password_label.pack()
-password_entry = tk.Entry(window, show="*", font=("Arial", 12), width=30)
+
+password_entry = tk.Entry(root, show="*", font=("Arial", 12), width=30)
 password_entry.pack()
 
-#  login button
-login_button = tk.Button(window, text="Login", command=login, font=("Arial", 13))
+login_button = tk.Button(root, text="Login", command=perform_login)
 login_button.pack()
 
-window.mainloop()
+# Bind the Enter key event to the perform_login function
+root.bind("<Return>", lambda event=None: perform_login())
+
+root.mainloop()
+
