6e4d7f8785630d6b4f701bfb575abfd774bd1abf
Trevor <82081705+TrevorMwangi@users.noreply.github.com>
2023-08-21 19:49:39 +0300

    the complete login page
    password generator included

--- loginpage.py
+++ loginpage.py
@@ -1,44 +1,130 @@
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import ttk
+import random
+import string
 
-def close_all_windows():
-    root.destroy()
+#Password generator
+def generate_password():
+    length = int(length_entry.get())
+    use_lowercase = lowercase_var.get()
+    use_uppercase = uppercase_var.get()
+    use_digits = digits_var.get()
+    use_special_chars = special_chars_var.get()
 
+    if not any([use_lowercase, use_uppercase, use_digits, use_special_chars]):
+        messagebox.showerror("Error", "Please select at least one option.")
+        return
+
+    chars = ""
+    if use_lowercase:
+        chars += string.ascii_lowercase
+    if use_uppercase:
+        chars += string.ascii_uppercase
+    if use_digits:
+        chars += string.digits
+    if use_special_chars:
+        chars += string.punctuation
+
+    generated_password = "".join(random.choice(chars) for _ in range(length))
+    new_password_entry.delete(0, tk.END)  # Clear previous content
+    new_password_entry.insert(0, generated_password)
+
+def open_password_generator():
+    login_frame.pack_forget()  # Hide the login frame
+    password_generator_frame.pack()  # Show the password generator frame
+
+
+#Login Function
 def perform_login():
     username = username_entry.get()
-    password = password_entry.get()
+    entered_password = password_entry.get()
 
-    if username == "Trevor" and password == "password123":
-        messagebox.showinfo("Login Successful", "Welcome, {}!".format(username))
+    if username == "user":
+        if entered_password == current_password or entered_password == new_password_entry.get():
+            messagebox.showinfo("Login Successful", "Welcome, {}!".format(username))
+            root.quit()
+        else:
+            messagebox.showerror("Login Failed", "Invalid username or password")
     else:
-        messagebox.showerror("Login Failed!", "Invalid username or password")
+        messagebox.showerror("Login Failed", "Invalid username")
 
-     # After showing the login result, ask for confirmation to close all windows
-    confirmation = messagebox.askokcancel("Close All Windows", "Do you want to close all windows?")
-    if confirmation:
-        close_all_windows()    
 
+#Main Window Frame
 root = tk.Tk()
 root.title("Login Form")
-root.geometry("500x300")
+root.geometry("500x400")
 
-username_label = tk.Label(root, text="Username:", font= ("Arial", 13))
-username_label.pack()
-
-username_entry = tk.Entry(root, font=("Arial", 12), width=30)
-username_entry.pack(pady=5)
-
-password_label = tk.Label(root, text="Password:", font=("Arial", 13))
-password_label.pack(pady=5)
-
-password_entry = tk.Entry(root, show="*", font=("Arial", 12), width=30)
-password_entry.pack(pady=5)
-
-login_button = ttk.Button(root, text="Login", command=perform_login)
-login_button.pack(padx=5)
+style = ttk.Style()
+custom_font = ("Helvetica", 12)  # Tuple containing font family and size
+style.configure("Custom.TButton", font=custom_font)
 
 
-# Bind the Enter key event to the perform_login function
+# Login Frame
+login_frame = tk.Frame(root)
+login_frame.pack(padx=20, pady=20)
+
+username_label = tk.Label(login_frame, text="Username:", font=("Arial", 14))
+username_label.grid(row=0, column=0, padx=10, pady=5, sticky="w")
+
+username_entry = tk.Entry(login_frame, font=("Arial", 14))
+username_entry.grid(row=0, column=1, padx=10, pady=5)
+
+password_label = tk.Label(login_frame, text="Password:", font=("Arial", 14))
+password_label.grid(row=1, column=0, padx=10, pady=5, sticky="w")
+
+password_entry = tk.Entry(login_frame, show="*", font=("Arial", 14))
+password_entry.grid(row=1, column=1, padx=10, pady=5,)
+
+login_button = ttk.Button(login_frame, text="Login", command=perform_login, style="Custom.TButton")
+login_button.grid(row=2, columnspan=2, padx=10, pady=20)
+
+password_generator_button = ttk.Button(login_frame, text="Forgot Password?", command=open_password_generator)
+password_generator_button.grid(row=3, columnspan=2, padx=10, pady=10)
+
+current_password = "password"  # The existing password
+
+
+# Password Generator Frame
+password_generator_frame = tk.Frame(root)
+
+length_label = tk.Label(password_generator_frame, text="Password Length:")
+length_label.pack(pady=10)
+
+length_entry = tk.Entry(password_generator_frame, font=("Arial", 13))
+length_entry.pack()
+
+lowercase_var = tk.BooleanVar()
+lowercase_check = tk.Checkbutton(password_generator_frame, text="Include Lowercase Letters", variable=lowercase_var)
+lowercase_check.pack(pady=5, anchor="w")
+
+uppercase_var = tk.BooleanVar()
+uppercase_check = tk.Checkbutton(password_generator_frame, text="Include Uppercase Letters", variable=uppercase_var)
+uppercase_check.pack(pady=5, anchor="w")
+
+digits_var = tk.BooleanVar()
+digits_check = tk.Checkbutton(password_generator_frame, text="Include Digits", variable=digits_var)
+digits_check.pack(pady=5, anchor="w")
+
+special_chars_var = tk.BooleanVar()
+special_chars_check = tk.Checkbutton(password_generator_frame, text="Include Special Characters", variable=special_chars_var)
+special_chars_check.pack(pady=5, anchor="w")
+
+generate_button = ttk.Button(password_generator_frame, text="Generate Password", command=generate_password, style="Custom.TButton")
+generate_button.pack(pady=10)
+
+new_password_label = tk.Label(password_generator_frame, text="Generated Password:")
+new_password_label.pack(pady=5)
+
+new_password_entry = tk.Entry(password_generator_frame, show="", font=("Arial", 12))  # Show generated password
+new_password_entry.pack()
+
+back_button = ttk.Button(password_generator_frame, text="Back to Login", command=lambda: [password_generator_frame.pack_forget(), login_frame.pack()], style="Custom.TButton")
+back_button.pack(pady=10, padx=10)
+
+new_password = ""  # Initialize the new password
+
+login_frame.pack()  # Display the login frame initially
+
 root.bind("<Return>", lambda event=None: perform_login())
 root.mainloop()
